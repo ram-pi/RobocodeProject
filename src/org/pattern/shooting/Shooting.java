8098e0f49254135e59b7cb3557764edd253d7484
veget88 <pierpaolo.rametta@gmail.com>
2014-05-13 11:54:25 +0200

    Adding prediction for shooting

--- src/org/pattern/shooting/Shooting.java
+++ src/org/pattern/shooting/Shooting.java
@@ -1,8 +1,13 @@
 package org.pattern.shooting;
 
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
+import org.pattern.movement.Projection;
+import org.pattern.movement.Projection.tickProjection;
 import org.robot.Enemy;
 
 import robocode.AdvancedRobot;
@@ -13,6 +18,7 @@ public class Shooting implements Observer {
 	private Enemy enemy;
 	private double bulletPower;
 	private ViewFinder sniper;
+	private Point2D shootingAt;
 
 	public Shooting(AdvancedRobot robot) {
 		this.robot = robot;
@@ -31,24 +37,39 @@ public class Shooting implements Observer {
 	}
 
 	public void doShooting(ScannedRobotEvent enemy) {
-		
+		firingSettings();
 		sniper.setRobot(robot);
-		Enemy e = new Enemy(enemy, robot);
-		sniper.setPointToShot(e.getX(), e.getY());
-		if (shouldShot(enemy)) {
+		if (findShootingPoint(enemy)) {
 			sniper.moveGunToPoint();
-			firingSettings(enemy);
 			robot.fire(this.bulletPower);
 		}
 	}
+	
+	public boolean findShootingPoint(ScannedRobotEvent enemy) {
+		Enemy e = new Enemy(enemy, this.robot);
+		Point2D enemyPosition = new Point2D.Double(e.getX(), e.getY());
+		int direction = (int) Math.signum(e.getVelocity());
+		if (direction == 0)
+			direction = 1;
+		Projection houdini = new Projection(enemyPosition, e.getHeading(), e.getVelocity(), direction, e.getHeading());
+		List<tickProjection> possibleTargets = houdini.projectNextTicks(2000);
+		for (int i = 0; i < possibleTargets.size(); i++) {
+			tickProjection tmpTick = possibleTargets.get(i);
+			double tmpDistance = Point.distance(robot.getX(), robot.getY(), tmpTick.getPosition().getX(), tmpTick.getPosition().getY());
+			if (Math.abs(i - tmpDistance/(20 - 3*bulletPower)) < 2 ) {
+				sniper.setPointToShot(tmpTick.getPosition().getX(), tmpTick.getPosition().getY());
+				return true;
+			}
+		}
+		return false;
+	}
+	
 
 	public boolean shouldShot(ScannedRobotEvent enemy) {
 		return true;
 	}
 
-	// Set the values for fire power, farther is the enemy slower is the bullet
-	public void firingSettings(ScannedRobotEvent enemy) {
-		//this.bulletPower = Math.min(400/enemy.getDistance(), 3.0);
+	public void firingSettings() {
 		this.bulletPower = Math.min(3.0, robot.getEnergy());
 	}
 }
