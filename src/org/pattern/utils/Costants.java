4558e7dafeeef4fb593d974df836c54271636947
garulf <garulf@autistici.org>
2014-06-08 10:50:30 +0200

    strategy and stick base on prediction in smoothing

--- src/org/pattern/utils/Costants.java
+++ src/org/pattern/utils/Costants.java
@@ -4,7 +4,7 @@ public class Costants {
 
 	public static final double SURFING_REMOVE_WAVE_OFFSET = 50;
 	public static int SEG_BITS_VARIABLE = 64;
-	public static int KNN_K = 15;
+	public static int KNN_K = 10;
 	public static int NUM_BINS = 43;
 	public static int SMOOTH_BINS = 4;
 	public static int POINT_MIN_DIST_ENEMY = 40;
@@ -14,7 +14,7 @@ public class Costants {
 	public static int FIREPOWER_ENERGY_TRESHOLD = 15;
 	public static double FIREPOWER_FP_UNDER_TRESHOLD = 1.;
 	public static double GUN_MAX_DISPLACEMENT_DEGREE = 3.9;
-	public static double STICK_LENGTH = 140;
+	public static double STICK_LENGTH = 160;
 	public static double MINIMUM_RADIUS = 114;
 	public static double SURFING_MAX_POINTS_DIST = 160;
 	public static double SURFING_MAX_DISTANE_HITTED_WAVE = 50;
@@ -22,5 +22,12 @@ public class Costants {
 	public static double RADAR_STICK_LENGTH = 140.0;
 	public static double SURFING_NUM_POINTS = 200;
 	public static double SURFING_MAX_POINT_DIST = 100;
+	public static final double DISTANCE_OFFSET = 7.;
 	public static final double SURFING_BULLET_HIT_BULLET_DISTANCE = 40;
+	public static final double ENERGY_RATIO_TAKE_DISTANCE = 1.5;
+	public static final double MIN_DISTANCE = 350;
+	public static final double ENERGY_RATIO_GO_NEAR = 3;
+	public static final double MAX_DISTANCE = 150;
+	public static final double ENERGY_RATIO_DONT_FIRE = 2.0;
+	public static final double DISTANCE_DONT_FIRE = 450;
 }
